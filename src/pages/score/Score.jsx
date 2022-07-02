0b526f4ed48af7f21fd6f92b40597abc51dad044
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2022-07-02 12:17:36 +0300

    feat: add score page

---
+++ src/pages/score/Score.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Score = () => {
+  return (
+    <div>
+      SCORE
+    </div>
+  );
+};
+
+export default Score;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/main/MainPage';
 import Settings from './../pages/settings/Settings';
 import Categories from './../pages/categories/Categories';
+import Score from './../pages/score/Score';
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Route path='/' element={<MainPage/>}/>
       <Route path='/settings' element={<Settings/>}/>
       <Route path='/categories' element={<Categories/>}/>
+      <Route path='/score' element={<Score/>}/>
     </Routes>
   );
 };
