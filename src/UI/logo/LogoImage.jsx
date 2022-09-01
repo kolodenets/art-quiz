9250d1a08b530bca71f9b80aa140c613a41c893f
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2022-09-01 12:47:52 +0300

    feat: add footer

--- src/UI/logo/LogoImage.jsx
+++ src/UI/logo/LogoImage.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import style from './LogoImage.module.css'
 
 const logo = '../logo.png'
 
 const LogoImage = ({width, margin}) => {
-  const navigate = useNavigate();
-  const openMainPage = () => {
-    navigate('/')
-  }
   return (
     <>
       <img
       style={{width: `${width}`, margin: `${margin}`}} 
-      onClick={openMainPage} 
       className={style.mainLogo} 
       src={logo} 
       alt="logo" />
